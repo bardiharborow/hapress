75f0ac6a08a07320517e5c05d153b53c7242097e
Bardi Harborow <bardi@bardiharborow.com>
2018-03-29 16:24:25 +1100

    Install Mercator 1.0.2.

---
+++ content/sunrise.php
@@ -0,0 +1,3 @@
+<?php
+defined( 'WPMU_PLUGIN_DIR' ) or define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' );
+require WPMU_PLUGIN_DIR . '/mercator/mercator.php';
--- wp-config.php
+++ wp-config.php
@@ -107,6 +107,11 @@ if (isset($_SERVER['MULTISITE']) ? $_SERVER['MULTISITE'] === 'true' : false) {
 	define( 'PATH_CURRENT_SITE', '/' );
 	define( 'SITE_ID_CURRENT_SITE', 1 );
 	define( 'BLOG_ID_CURRENT_SITE', 1 );
+
+	/**
+	 * Enable domain mapping.
+	 */
+	define( 'SUNRISE', true );
 }
 
 /**
